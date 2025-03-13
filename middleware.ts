8270fb5052072974545b4a0e41c1aa0ee0cb7560
Tim <timblazic.info@gmail.com>
2025-03-13 18:54:24 +0100

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -35,6 +35,6 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // Skip all internal paths (_next, api, etc)
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico).*)/public",
   ],
 };
