4160bad32f63e5ae56bec25cb9384b045d989e3e
Tim <timblazic.info@gmail.com>
2025-03-13 20:07:58 +0100

    email change

--- app/[lang]/kontakt/page.tsx
+++ app/[lang]/kontakt/page.tsx
@@ -89,7 +89,7 @@ export default function ContactPage() {
                     href="mailto:damjan@dm-metal.eu"
                     className="text-red-600 hover:text-red-700 font-semibold"
                   >
-                    damjan@dmwmetal.eu
+                    damjan@dmmetal.eu
                   </a>
                 </div>
               </div>
